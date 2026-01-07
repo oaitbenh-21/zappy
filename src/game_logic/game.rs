8c3bf59930a74886a925720c793470ee2c10a590
omar ait benhammou <0xmrrandom@gmail.com>
2026-01-07 08:37:29 +0100

    add some methods but the structure not completed yet

--- src/game_logic/game.rs
+++ src/game_logic/game.rs
@@ -1,11 +1,32 @@
+use std::collections::HashMap;
+
+use crate::game_logic::game;
+
 use super::player::Player;
 use super::stone::Item;
 use uuid::Uuid;
 
 pub struct Game {
     pub map: Vec<Vec<u8>>,
-    pub players: Vec<Player>,
+    pub players: HashMap<Uuid, Player>,
     pub families: Vec<Uuid>,
+}
+
+impl Game {
+    pub fn new() -> Game {
+        Game {
+            map: Vec::new(),
+            players: HashMap::new(),
+            families: Vec::new(),
+        }
+    }
+
+    pub fn add_player(&mut self, player: Uuid) {
+        self.players.insert(player, Player::new());
+    }
+    pub fn kill_player(&mut self, player: Uuid) {
+        self.players.remove(&player);
+    }
 }
 
 pub struct Tile {
@@ -13,4 +34,15 @@ pub struct Tile {
     pub players: Vec<Uuid>,
     pub x: usize,
     pub y: usize,
+}
+
+impl Tile {
+    pub fn new(row: usize, col: usize) -> Tile {
+        Tile {
+            objects: Vec::new(),
+            players: Vec::new(),
+            x: col,
+            y: row,
+        }
+    }
 }
--- src/game_logic/player.rs
+++ src/game_logic/player.rs
@@ -1,12 +1,45 @@
 use std::collections::HashMap;
 use uuid::Uuid;
 
+use crate::game_logic::stone::Item;
+
 use super::stone::Stone;
 
 pub struct Player {
     pub x: usize,
     pub y: usize,
     pub food: u32,
+    pub level: u8,
     pub stones: HashMap<Stone, u32>,
     pub family: Uuid,
+}
+
+impl Player {
+    pub fn new() -> Player {
+        return Player {
+            x: 0,
+            y: 0,
+            food: 0,
+            level: 1,
+            stones: HashMap::new(),
+            family: Uuid::new_v4(),
+        };
+    }
+    pub fn fork(family: Uuid, row: usize, col: usize) -> Player {
+        return Player {
+            x: col,
+            y: row,
+            food: 0,
+            level: 1,
+            stones: HashMap::new(),
+            family: family,
+        };
+    }
+    pub fn eat(&mut self, item: Item) {
+        if (matches!(item, Item::Food)) {
+            // handle logic of eat food(time coin)
+            return;
+        }
+        // handle logic of stone eat and calculate level of player
+    }
 }
